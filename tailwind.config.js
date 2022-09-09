9f729e0a956ab8a1e038d969d6b45663cb07b902
Afrndyyy1 <araiz5021@gmail.com>
2022-09-09 13:33:45 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        'Inter' : ['Inter'],
+      },
+      screen: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
